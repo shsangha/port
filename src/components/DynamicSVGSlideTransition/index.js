fd8124e5e2cf6a18540d99aa0dc2fc4edc061731
shsangha <sssangha@ucalgary.ca>
2019-08-07 14:01:18 -0600

    generalized path data into one fn, need to pass fns as a render prop to be used in index page when transitioning

--- src/components/DynamicSVGSlideTransition/index.js
+++ src/components/DynamicSVGSlideTransition/index.js
@@ -4,69 +4,30 @@ import TransitionLink from "gatsby-plugin-transition-link"
 
 import styles from "./style.module.scss"
 
-const right = () =>
-  `M${2000},-1000  Q${800} -500 ${400} 0 Q${0} ${500} ${400} 500 T ${400} 1000 Q${800} 1500 ${2000} 2000H2000V-1000Z`
+const setPathData = (top, left, yPosition) => {
+  const controlPoint1X = (top && left) || (!top && !left) ? 0 : 1000
+  const controlPoint2X = controlPoint1X ? 0 : 1000
 
-const left = (offset, direcction) => {
-  const half = offset / 2
-  const up = direcction === "up"
+  const direction = left ? -1 : 1
 
-  const controlPoint1X = up ? 0 : offset
-  const controlPoint2X = up ? offset : 0
-  const controlPoint3X = controlPoint2X
-
-  return `M-2000,-1000  Q${
-    controlPoint1X /* this flips the top axis*/
-  } -500 ${half} 0 Q${
-    controlPoint2X /*this flips the main axis */
-  } ${half} ${half} 500 T ${half} 1000 Q${
-    controlPoint3X /*this flips the bottom axis */
-  } 1500 -2000 2000Z    `
-}
-
-const setPathData = (top, left, xOffset, yPosition) => {
-  const half = xOffset / 2
-
-  const controlPoint1X = (top && left) || (!top && !left) ? 0 : xOffset
-  const controlPoint2X = controlPoint1X ? 0 : xOffset
-
-  return left
-    ? `M-2000,-1000  Q${
-        controlPoint1X /* this flips the top axis*/
-      } ${-1000} ${half} 0 Q${
-        controlPoint2X /*this flips the main axis */
-      } ${500} ${half} 500 T ${half} 1000 Q${
-        controlPoint2X /*this flips the bottom axis */
-      } 1500 -2000 2000Z `
-    : `M2000,-1000
-       Q${controlPoint1X} -${yPosition}
-       ${half} 0 
-       Q${controlPoint2X} ${yPosition}
-       ${half} 500 
-       T ${half} ${1000}
-       Q${controlPoint2X} ${yPosition + 1000}
-       2000 2000
-       `
+  return `M${2000 * direction},-1000 
+          Q${controlPoint1X} -${yPosition}
+          500 0 
+          Q${controlPoint2X} ${yPosition}
+          500 500
+          T 500 1000
+          Q${controlPoint2X} ${yPosition + 1000}
+          ${2000 * direction} 2000Z `
 }
 
 export default class DynamicSVGSlide extends Component {
   layer1Ref = createRef()
   layer2Ref = createRef()
 
-  state = {
-    offset: this.calculateOffset(),
-  }
-
-  calculateOffset() {
-    const width = window.innerWidth
-
-    return width < 300 ? 1200 : width < 600 ? 1000 : width < 900 ? 900 : 800
-  }
-
   componentDidMount() {
     TweenLite.set("#layer1", {
       attr: {
-        d: setPathData(1, 0, 300, 500),
+        d: setPathData(1, 1, 300),
       },
     })
   }
@@ -86,7 +47,7 @@ export default class DynamicSVGSlide extends Component {
 
     tl.set("#layer1", {
       attr: {
-        d: right(xOffset, "up"),
+        //d: right(xOffset, "up"),
       },
       x: -800,
     }).to("#layer1", 4, {
@@ -109,12 +70,12 @@ export default class DynamicSVGSlide extends Component {
         >
           Link
         </TransitionLink>
-        <svg viewBox="-1000 -1000 4000 4000" preserveAspectRatio="none">
+        <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
           <path
             d={""}
             id="layer1"
             ref={this.layer1Ref}
-            fill="none"
+            fill="blue"
             stroke="black"
             strokeMiterlimit="100"
           />
@@ -131,3 +92,29 @@ export default class DynamicSVGSlide extends Component {
     )
   }
 }
+
+/* 
+
+
+ the seperated path data
+
+  return left
+    ? `M-2000,-1000 
+       Q${controlPoint1X} -${yPosition}
+       ${half} 0 
+       Q${controlPoint2X} ${yPosition}
+       ${half} 500
+       T ${half} 1000
+       Q${controlPoint2X} ${yPosition + 1000}
+      -2000 2000Z `
+    : `M2000,-1000
+       Q${controlPoint1X} -${yPosition}
+       ${half} 0 
+       Q${controlPoint2X} ${yPosition}
+       ${half} 500 
+       T ${half} ${1000}
+       Q${controlPoint2X} ${yPosition + 1000}
+       2000 2000Z
+       `
+
+       */
